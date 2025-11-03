d7037fa740da256a7a7d6a822ab7f697b4a30257
YAMINA-2109 <yaminaatmaoui@gmail.com>
2025-11-03 20:10:36 +0100

    Track actual contents of 1-fastapi_revision as regular files

---
+++ 1-fastapi_revision/backend/core/config.py
@@ -0,0 +1,5 @@
+class Settings:
+    PROJECT_NAME: str = "My FastAPI Application"
+    PROJECT_VERSION: str = "0.1.0"
+
+settings = Settings()
---
+++ 1-fastapi_revision/backend/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from core.config import settings
+
+app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+
+
+@app.get("/")
+def home_page():
+    return {"message": "Welcome to My FastAPI Application!"}   
