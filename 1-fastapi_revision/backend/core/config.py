8a7f2f5f09c539bc45551347e009f80b788c8456
YAMINA-2109 <yaminaatmaoui@gmail.com>
2025-11-07 23:14:13 +0100

    DB: Configure the db

--- 1-fastapi_revision/backend/core/config.py
+++ 1-fastapi_revision/backend/core/config.py
@@ -1,5 +1,21 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+env_path = Path('.') / ".env"
+load_dotenv(dotenv_path=env_path)
+
 class Settings:
     PROJECT_NAME: str = "My FastAPI Application"
     PROJECT_VERSION: str = "0.1.0"
+
+    POSTEGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTEGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    POSTEGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
+    POSTEGRES_PORT: str = os.getenv("POSTGRES_PORT", "5432")
+    POSTEGRES_DB: str = os.getenv("POSTGRES_DB")
+    DATABASE_URL: str = f"postgresql://{POSTEGRES_USER}:{POSTEGRES_PASSWORD}@{POSTEGRES_SERVER}:{POSTEGRES_PORT}/{POSTEGRES_DB}"
+
+
 
 settings = Settings()
---
+++ 1-fastapi_revision/backend/db/session.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from core.config import settings
+
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+print(f"Database URL: {SQLALCHEMY_DATABASE_URL}")
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SESSIONLOCAL = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# sqlite
+# SQLALCHEMY_DATABASE_URL_SQLITE = "sqlite:///./sql_app.db"
+# engine_sqlite = create_engine(
+#     SQLALCHEMY_DATABASE_URL_SQLITE, connect_args={"check_same_thread": False}
+# )
